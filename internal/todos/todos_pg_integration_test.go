3f25cb38ebf9a706539e0ab82dd6a467bcb087eb
daalfox <daalfox@gmail.com>
2025-06-22 12:53:52 +0330

    Refactor

--- internal/todos/todos_pg_integration_test.go
+++ internal/todos/todos_pg_integration_test.go
@@ -17,35 +17,7 @@ import (
 func TestPostgresRepo(t *testing.T) {
 	t.Run("test crud", func(t *testing.T) {
 		ctx := context.Background()
-		postgresContainer, err := postgres.Run(
-			ctx,
-			"postgres:17.5-alpine",
-			postgres.WithInitScripts(filepath.Join("..", "..", "migrations", "20250622081024_init.sql")),
-			postgres.WithDatabase("test"),
-			postgres.WithUsername("user"),
-			postgres.WithPassword("password"),
-			testcontainers.WithWaitStrategy(
-				wait.ForLog("database system is ready to accept connections").WithOccurrence(2).WithStartupTimeout(5*time.Second)),
-		)
-		if err != nil {
-			t.Fatal(err)
-		}
-		t.Cleanup(func() {
-			if err := postgresContainer.Terminate(ctx); err != nil {
-				t.Fatalf("failed to terminate pgContainer: %s", err)
-			}
-		})
-
-		connStr, err := postgresContainer.ConnectionString(ctx)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		conn, err := pgx.Connect(ctx, connStr)
-		if err != nil {
-			t.Fatal(err)
-		}
-
+		conn := getTestPostgresConn(t, ctx)
 		todoRepo := todos.NewPostgresRepo(ctx, conn)
 
 		todo := todos.Todo{
@@ -60,4 +32,36 @@ func TestPostgresRepo(t *testing.T) {
 
 		assert.Equal(t, todo, todoFromDb)
 	})
+}
+
+func getTestPostgresConn(t testing.TB, ctx context.Context) *pgx.Conn {
+	t.Helper()
+	postgresContainer, err := postgres.Run(
+		ctx,
+		"postgres:17.5-alpine",
+		postgres.WithInitScripts(filepath.Join("..", "..", "migrations", "20250622081024_init.sql")),
+		postgres.WithDatabase("test"),
+		postgres.WithUsername("user"),
+		postgres.WithPassword("password"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").WithOccurrence(2).WithStartupTimeout(5*time.Second)),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		if err := postgresContainer.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate pgContainer: %s", err)
+		}
+	})
+
+	connStr, err := postgresContainer.ConnectionString(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	conn, err := pgx.Connect(ctx, connStr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return conn
 }
